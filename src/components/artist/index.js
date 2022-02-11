d6e7034667188b038fecbf4767e8ce5973153917
Haseeb-daar <abdul.haseeb@purevpn.com>
2022-02-11 12:00:36 +0500

    fix team issues

--- src/components/artist/index.js
+++ src/components/artist/index.js
@@ -1,8 +1,9 @@
 import PIC1 from '../../assets/003.png'
-
+import { useHistory } from 'react-router-dom';
 import './style.css'
 
 function Artist() {
+    const history = useHistory()
     return (
         <div id='artist' className="artist_container">
             <div className='main_heading_wrapper'>
@@ -16,7 +17,7 @@ function Artist() {
                     <h1 className='theme_heading'> PETER DENNELIS</h1>
                     <p className='theme_text'>Peter worked for famous studios such as Disney, Warner Bros, Universal and other well-known studios. If you’ve seen Jurassic Word (2018), Suicide Squad (2016)
                     </p>
-                    <button onClick={() => window.location.href = '/team'} className='theme_button'>Read More</button>
+                    <button onClick={() => history.push('/team')} className='theme_button'>Read More</button>
                 </div>
                 <div className='img_contaier'>
                     <img alt='bear' loading='lazy' src={PIC1} />
--- src/config/router.js
+++ src/config/router.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "../components/pages/home";
 import Team from "../components/pages/team";
+import ScrollToTop from "./ScrollToTop";
 
 function AppRouter(){
     return(
         <Router>
+             <ScrollToTop />
             <Switch>
-                <Route exact path={'/'} component={App}/>
+                <Route  exact path={'/'} component={App}/>
                 <Route exact path={'/team'} component={Team}/>
 
             </Switch>
